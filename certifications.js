2aeda729875609a5615db8767f8e8733e73ea1aa
moktansujanj5 <moktansujanj5@gmail.com>
2025-03-06 03:02:06 +0545

    update: added new certification

--- certifications.js
+++ certifications.js
@@ -1,5 +1,11 @@
 let certifications = [
     {
+        'image': 'assets/pictures/icons/simplilearn.png',
+        'title': 'Agile Scrum Master (Basic)',
+        'from': 'Simplilearn',
+        'url': 'https://simpli-web.app.link/e/Sek4Fc2YuRb'
+    },
+    {
         'image': 'assets/pictures/icons/hacker-rank.png',
         'title': 'Java (Basic)',
         'from': 'Hacker Rank',
